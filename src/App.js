641727bf50fdfad9419f9015a38aba6ec1afa0a7
SECONDTA ARDIANSYAH <idnssquad300@gmail.com>
2024-05-07 10:13:49 +0700

    ADD condition at App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import SidebarComp from "./components/Sidebar";
 import Dashboard from "./pages/Dashboard";
 import Device from "./pages/Device";
 import Login from "../src/auth/Login";
@@ -15,13 +14,20 @@ import Agen from "./pages/Agen";
 import ChatAgen from "./pages/ChatAgen";
 
 function App() {
+  const path = window.location.pathname.split("/")[1];
+  const isDashboard = path === "dashboard";
+  const contentClass = isDashboard
+    ? "p-4 mt-12 sm:ml-64 bg-[#F4F4F4] min-h-screen"
+    : "";
+
   useEffect(() => {
     initFlowbite();
   }, []);
+
   return (
     <>
-      <SidebarComp />
-      <div className="p-4 mt-12 sm:ml-64 bg-[#F4F4F4] min-h-screen">
+      {isDashboard && <SidebarComp />}
+      <div className={contentClass}>
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<Home />} />
